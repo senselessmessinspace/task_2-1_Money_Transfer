d57716c9139fed923925d1cffb8690a3acc6ac7d
Oxana Gunchenko <ksuper2007@gmail.com>
2021-07-25 17:30:43 +0300

    Initial commit. Add Money_Transfer.java

---
+++ Money_Transfer.java
@@ -0,0 +1,9 @@
+public class Money_Transfer {
+    public static void main(String[] args) {
+      int currentAccountMoney = 2_000_000_000;
+      int additionalMoney = 500_000_000;
+      int currentAccountResult = currentAccountMoney + additionalMoney;
+      System.out.println(currentAccountResult);
+    }
+  }
+  
